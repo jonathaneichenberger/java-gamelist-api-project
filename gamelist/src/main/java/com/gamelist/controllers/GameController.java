bdb7dac7b25d4a5a0267949b5d98519c066be04f
Jonathan Eichenberger <djohnni@hotmail.com>
2024-11-14 09:37:20 -0300

    Update GameController.java

--- gamelist/src/main/java/com/gamelist/controllers/GameController.java
+++ gamelist/src/main/java/com/gamelist/controllers/GameController.java
@@ -1,16 +1,16 @@
-package com.devsuperior.dslist.controllers;
+package com.gamelist.controllers;
 
 import java.util.List;
 
+import com.gamelist.dto.GameDTO;
+import com.gamelist.dto.GameMinDTO;
+import com.gamelist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dslist.dto.GameDTO;
-import com.devsuperior.dslist.dto.GameMinDTO;
-import com.devsuperior.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/games")
