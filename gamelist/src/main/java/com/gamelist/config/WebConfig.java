e2a3b4fc679ef16eaa95e9143274913b6d649d5e
Jonathan Eichenberger <djohnni@hotmail.com>
2025-02-03 08:32:07 -0300

    Atualizando WebConfig para usar a API com Angular

--- gamelist/src/main/java/com/gamelist/config/WebConfig.java
+++ gamelist/src/main/java/com/gamelist/config/WebConfig.java
@@ -1,25 +1,17 @@
 package com.gamelist.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${cors.origins}")
-    private String corsOrigins;
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")  // Allow your Angular app
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
-
 }
